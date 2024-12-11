a3e8000010340e601e2fc145dfb1cbdc9f4df9c0
chee loong <loongest@gmail.com>
2024-12-12 00:11:08 +0800

    test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,16 +10,16 @@ const config: Config = {
   favicon: 'img/favicon.ico',
   // Set the production url of your site here
   //url: 'https://your-docusaurus-site.example.com',
-  url: 'https://github.com',
+  url: 'https://loongest.github.io',
+
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/resume.github.io/',
+  baseUrl: '/resume/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'loongest', // Usually your GitHub org/user name.
-  projectName: 'resume.github.io', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  projectName: 'resume', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
