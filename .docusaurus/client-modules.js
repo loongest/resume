04073a055a642be84a3818cd94073d5d8b84ba80
chee loong <loongest@gmail.com>
2024-12-12 12:19:50 +0800

    Deploy website - based on 20f86a2c63f8f08f8945511495bc7c985e9d05c7

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/cheemingloong/Documents/workspace-singuhe/resume/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/cheemingloong/Documents/workspace-singuhe/resume/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/cheemingloong/Documents/workspace-singuhe/resume/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/cheemingloong/Documents/workspace-singuhe/resume/src/css/custom.css"),
-];
